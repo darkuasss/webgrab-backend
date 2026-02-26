6684c6fc7a7570d2b44d024a6b68c79f9c561436
darkuasss <christoph.frick@cloud-content.online>
2026-02-26 08:37:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -77,8 +77,10 @@ async def analyze(data: dict):
     try:
         # Starte den unsichtbaren Browser
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=True)
-            
+            browser = await p.chromium.launch(
+    headless=True,
+    args=["--no-sandbox", "--disable-dev-shm-usage"]
+)
             # Simuliere einen echten Windows/Chrome-Nutzer
             context = await browser.new_context(
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
